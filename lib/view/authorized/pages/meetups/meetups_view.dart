46246954f1a5b7a2848d2237f41bedde5db04f09
Yunus Karaaslan <karaaslan.21@hotmail.com>
2022-03-10 06:50:01 +0300

    added meeting ui

--- lib/view/authorized/pages/meetups/meetups_view.dart
+++ lib/view/authorized/pages/meetups/meetups_view.dart
@@ -1,3 +1,4 @@
+import 'package:atalay/core/constant/styles.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
@@ -18,7 +19,86 @@ class MeetupsView extends StatelessWidget {
         zoomDrawerController: zoomDrawerController,
         title: 'meetups'.tr(),
       ),
-      onPageBuilder: (context, value) => Container(),
+      onPageBuilder: (context, value) => const _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 15,
+      itemBuilder: (context, index) => InkWell(
+        onTap: () => true,
+        child: Card(
+          elevation: 4,
+          color: Colors.lightGreen,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+          child: ClipPath(
+            clipper: ShapeBorderClipper(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Container(
+              color: Colors.white,
+              margin: const EdgeInsets.only(left: 10),
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Takim Toplantisi',
+                          style: cardTitleStyle(),
+                        ),
+                      ),
+                      const CircleAvatar(
+                        radius: 15,
+                        backgroundImage: NetworkImage(
+                            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'),
+                      ),
+                      const SizedBox(width: 15),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.timelapse,
+                        color: Colors.amber,
+                      ),
+                      SizedBox(width: 10),
+                      Text('22 Mart 2022 Sali, 10:30 - 11:30'),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.location_city,
+                        color: Colors.redAccent,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Main Conference Hall, 3rd floor'),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
